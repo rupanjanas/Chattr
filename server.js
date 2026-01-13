203bb4dec5ce7099d16f426f4394ca1d5ad54361
rupanjanas <saharupanjana04@gmail.com>
2026-01-13 19:13:17 +0530

    update

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: ["http://localhost:5183"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST"],
     credentials: true,
   },
