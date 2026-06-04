fe4b9e0c424e6a37bd8a3f1dcc0f00b4e7248b56
Rupanjana <saharupanjana04@gmail.com>
2026-06-04 11:20:54 +0530

    Fix response handling in auth middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -20,7 +20,7 @@ const protect = async (req, res, next) => {
 
       next();
     } catch (error) {
-      res.status(401).json({ message: "Not authorized, token failed" });
+      return res.status(401).json({ message: "Not authorized, token failed" });
     }
   }
   else {
