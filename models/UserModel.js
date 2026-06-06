ea15c7e0a969cdd8f447e5cc52933c2b64c3ce70
Rupanjana <saharupanjana04@gmail.com>
2026-06-07 00:10:12 +0530

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-//schema
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -28,7 +27,7 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
-//! Hash user password before saving
+
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     return next();
@@ -36,7 +35,6 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, 10);
 });
 
-//Method to compare password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
