3d2b05adc38dfdebeac9c1070c5bd49d3da1b200
Rupanjana <saharupanjana04@gmail.com>
2026-06-13 17:51:13 +0530

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,38 +1,46 @@
 const express = require("express");
 const Message = require("../models/chatModel");
 const { protect } = require("../middleware/authMiddleware");
-
 const messageRouter = express.Router();
 
-// Send a message
-messageRouter.post("/", protect, async (req, res) => {
-  try {
-    const { content, groupId } = req.body;
-    const message = await Message.create({
-      sender: req.user._id,
-      content,
-      group: groupId,
-    });
-    const populatedMessage = await Message.findById(message._id).populate(
-      "sender",
-      "username email"
-    );
-    res.json(populatedMessage);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+const createMessageRouter = (io) => {
+  // Send a message
+  messageRouter.post("/", protect, async (req, res) => {
+    try {
+      const { content, groupId } = req.body;
+      const message = await Message.create({
+        sender: req.user._id,
+        content,
+        group: groupId,
+      });
 
-// Get messages for a group
-messageRouter.get("/:groupId", protect, async (req, res) => {
-  try {
-    const messages = await Message.find({ group: req.params.groupId })
-      .populate("sender", "username email")
-      .sort({ createdAt: 1 });
-    res.json(messages);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
+      const populatedMessage = await Message.findById(message._id).populate(
+        "sender",
+        "username email"
+      );
 
-module.exports = messageRouter;
+      if (io) {
+        io.to(groupId).emit("message received", populatedMessage);
+      }
+
+      res.json(populatedMessage);
+    } catch (error) 
+      res.status(400).json({ message: error.message });
+    }
+  });
+
+  messageRouter.get("/:groupId", protect, async (req, res) => {
+    try {
+      const messages = await Message.find({ group: req.params.groupId })
+        .populate("sender", "username email")
+        .sort({ createdAt: 1 });
+      res.json(messages);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
+
+  return messageRouter;
+};
+
+module.exports = createMessageRouter;
